a2ba3e8252a99b471ff8945daa01dfbb2fb03180
Võ Quang Hòa <voquanghoa@gmail.com>
2016-05-25 00:36:41 +0700

    Add project

---
+++ Excel2Json2/Program.cs
@@ -0,0 +1,79 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Excel2Json2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			if (args.Length == 0)
+			{
+				Console.WriteLine("Usage : <programme> <excel file>");
+				return;
+			}
+
+			var excelFileName = args[0];
+			var directory = Path.GetDirectoryName(excelFileName);
+			var jsonFileName = Path.Combine(directory, Path.GetFileNameWithoutExtension(excelFileName));
+			var dictData = new Dictionary<string, Data>()
+			{
+				{ "ENG", new Data() },
+				{ "VIE", new Data() },
+				{ "GER", new Data() },
+			};
+
+			var sheetNames = new[] { "Normal", "Hard" };
+
+			var sheets = Excel.Workbook.Worksheets(excelFileName).Where(x=>x.Rows.Length>0).ToList();
+
+			for(int i = 0; i < sheets.Count; i++)
+			{
+				foreach (var item in dictData)
+				{
+					item.Value.Add(new Level(sheetNames[i]));
+				}
+
+				var sheet = sheets[i];
+				int rowIndex = 0;
+				var lastAnswer = "";
+
+				foreach (var row in sheet.Rows.Skip(1))
+				{
+					rowIndex++;
+					var language = row.Cells[1].Text;
+					var question = row.Cells[2].Text;
+					var answer = row.Cells[3].Text;
+
+					if (string.IsNullOrEmpty(answer))
+					{
+						answer = lastAnswer;
+					}
+					else
+					{
+						lastAnswer = answer;
+					}
+					if (!string.IsNullOrEmpty(question))
+					{
+						dictData[language][i].Questions.Add(new Question()
+						{
+							Answer = answer,
+							Title = question
+						});
+					}
+				}
+			}
+
+			foreach (var item in dictData)
+			{
+				var fileName = string.Format("{0}_{1}.json", jsonFileName, item.Key);
+				File.WriteAllText(fileName, JsonConvert.SerializeObject(item.Value));
+			}
+		}
+	}
+}
---
+++ Excel2Json2/model.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excel2Json2
+{
+	[DataContract]
+	public class Question
+	{
+		[DataMember(Name = "title")]
+		public string Title
+		{
+			get; set;
+		}
+
+		[DataMember(Name = "answer")]
+		public string Answer
+		{
+			get; set;
+		}
+	}
+
+	[DataContract]
+	public class Level
+	{
+		[DataMember(Name = "name")]
+		public string Name
+		{
+			get; set;
+		}
+
+		[DataMember(Name = "questions")]
+		public List<Question> Questions
+		{
+			get; set;
+		}
+
+		public Level(string name)
+		{
+			Name = name;
+			Questions = new List<Question>();
+		}
+	}
+
+	[DataContract]
+	public class Data : List<Level>
+	{
+
+	}
+}
